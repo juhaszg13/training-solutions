a0a553ed9ad44e44431020aeac8b30821f63058e
juhaszg13 <juhaszg13@gmail.com>
2020-10-31 21:47:13 +0100

    Commit7

---
+++ src/main/java/classstructuremethods/NoteMain.java
@@ -0,0 +1,23 @@
+package classstructuremethods;
+
+
+import java.util.Scanner;
+
+public class NoteMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Note note=new Note();
+        System.out.println("Név:");
+        String name=scanner.nextLine();
+        note.setName(name);
+
+        System.out.println("Téma:");
+        String topic=scanner.nextLine();
+        note.setTopic(topic);
+
+        System.out.println("Szőveg:");
+        String text=scanner.nextLine();
+        note.setText(text);
+        System.out.println(note.getNoteText());
+    }
+}
