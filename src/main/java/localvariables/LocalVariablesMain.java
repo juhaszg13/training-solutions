7009d06f5b27a85b44992ac2013ea067e840e64f
juhaszg13 <juhaszg13@gmail.com>
2020-11-01 21:50:00 +0100

    Commit10

---
+++ src/main/java/localvariables/LocalVariablesMain.java
@@ -0,0 +1,18 @@
+package localvariables;
+
+public class LocalVariablesMain {
+    public static void main(String[] args) {
+        boolean b = false;
+        int a = 2;
+        int i = 3, j = 4;
+        int k = i;
+        String s = "Hello World!";
+        String t = s;
+        {
+            int x = 16;
+            System.out.println(a);
+        }
+
+    }
+}
+
