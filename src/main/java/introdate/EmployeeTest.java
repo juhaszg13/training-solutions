c991d144da983d9d568736af1edee93166d22b2c
juhaszg13 <juhaszg13@gmail.com>
2020-11-03 19:18:51 +0100

    Commit22

---
+++ src/main/java/introdate/EmployeeTest.java
@@ -0,0 +1,16 @@
+package introdate;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+
+        Employee employee=new Employee(1960,3,11,"Kis pista");
+        System.out.println("Neve:"+employee.getName());
+        System.out.println("Születési dátuma: "+employee.getDateOfBirth());
+        System.out.println("Felvétel dátuma: "+employee.getBeginEmployment());
+
+
+
+
+    }
+}
+
