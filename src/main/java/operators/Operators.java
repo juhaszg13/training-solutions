54d7c933fe9fc6c22255da9aa2e41d502862803f
juhaszg13 <juhaszg13@gmail.com>
2020-11-09 22:49:22 +0100

    Commit29

---
+++ src/main/java/operators/Operators.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class Operators {
+    boolean isEven(int n) {
+        if (n % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
