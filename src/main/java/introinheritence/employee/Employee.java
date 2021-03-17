961a21f89ab2832a86ac0cffd9c97d08942e6804
juhaszg13 <juhaszg13@gmail.com>
2021-03-17 17:11:13 +0100

    introinheritance_employee

---
+++ src/main/java/introinheritence/employee/Employee.java
@@ -0,0 +1,18 @@
+package introinheritence.employee;
+
+public class Employee extends Person {
+    private double salary;
+
+    public Employee(String name, String address, double salary) {
+        super(name, address);
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(int percent) {
+        salary *= 1.0 + percent / 100.0;
+    }
+}
