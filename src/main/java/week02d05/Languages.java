f34e83b03a74d3acced57f5b2191fb459f35e372
juhaszg13 <66171665+juhaszg13@users.noreply.github.com>
2021-04-18 16:17:06 +0200

    week02d05_Languages

---
+++ src/main/java/week02d05/Languages.java
@@ -0,0 +1,18 @@
+package week02d05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Languages {
+    public static void main(String[] args) {
+        List<String> languages = new ArrayList<>();
+        languages.add("Java");
+        languages.add("Python");
+        languages.add("JavaScript");
+        for (String s : languages) {
+            if (s.length() > 5) {
+                System.out.println(s);
+            }
+        }
+    }
+}
